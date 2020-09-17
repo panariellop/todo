1e918b910eec9e2b928f355c78d588b19878930d
panariellop <pierodpanariello@gmail.com>
2020-09-16 20:47:59 -0400

    updated scripts

---
+++ client/build/precache-manifest.ae05533bfcd546c3e8d4a9612cb6f188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3b75cf6e02c6c771b587df9a8ef542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ac66ab588950db843a",
+    "url": "/static/css/main.e5f7465f.chunk.css"
+  },
+  {
+    "revision": "9c110e53571bfb66e4d0",
+    "url": "/static/js/2.4f0b64b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4f0b64b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ac66ab588950db843a",
+    "url": "/static/js/main.70d12959.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
